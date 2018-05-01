bb68f09f7239c6d78c1a1b4ac72ce8bd00b0f7cc
Andriy Rymar <andriyrav@gmail.com>
2018-05-01 14:21:35 +0300

    Initial commit....this will be long way to perfection

---
+++ atomix-fighter/src/main/java/org/ar/atomix/fighters/AtomixFighterApplication.java
@@ -0,0 +1,12 @@
+package org.ar.atomix.fighters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomixFighterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AtomixFighterApplication.class, args);
+  }
+}
